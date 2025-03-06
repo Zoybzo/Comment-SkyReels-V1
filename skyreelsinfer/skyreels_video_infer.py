8a6abd7385ca409c88799a5fb026dd7ac834531b
Mkpk <user@BJ206116.local>
2025-03-06 10:16:50 +0800

    c

--- skyreelsinfer/skyreels_video_infer.py
+++ skyreelsinfer/skyreels_video_infer.py
@@ -25,7 +25,8 @@ logger.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
-    f"%(asctime)s - %(name)s - %(levelname)s - [%(filename)s:%(lineno)d - %(funcName)s] - %(message)s"
+    f"%(asctime)s - %(name)s - %(levelname)s - [%(filename)s:%(lineno)d - %("
+    f"funcName)s] - %(message)s"
 )
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
@@ -33,13 +34,15 @@ logger.addHandler(console_handler)
 
 class SkyReelsVideoSingleGpuInfer:
     def _load_model(
-        self,
-        model_id: str,
-        base_model_id: str = "hunyuanvideo-community/HunyuanVideo",
-        quant_model: bool = True,
-        gpu_device: str = "cuda:0",
+            self,
+            model_id: str,
+            base_model_id: str = "hunyuanvideo-community/HunyuanVideo",
+            quant_model: bool = True,
+            gpu_device: str = "cuda:0",
     ) -> SkyreelsVideoPipeline:
-        logger.info(f"load model model_id:{model_id} quan_model:{quant_model} gpu_device:{gpu_device}")
+        logger.info(
+            f"load model model_id:{model_id} quan_model:{quant_model} "
+            f"gpu_device:{gpu_device}")
         text_encoder = LlamaModel.from_pretrained(
             base_model_id,
             subfolder="text_encoder",
@@ -50,6 +53,7 @@ class SkyReelsVideoSingleGpuInfer:
             # subfolder="transformer",
             torch_dtype=torch.bfloat16,
             device="cpu",
+            # local_files_only=True,
         ).to("cpu")
         if quant_model:
             quantize_(text_encoder, float8_weight_only(), device=gpu_device)
@@ -69,15 +73,15 @@ class SkyReelsVideoSingleGpuInfer:
         return pipe
 
     def __init__(
-        self,
-        task_type: TaskType,
-        model_id: str,
-        quant_model: bool = True,
-        local_rank: int = 0,
-        world_size: int = 1,
-        is_offload: bool = True,
-        offload_config: OffloadConfig = OffloadConfig(),
-        enable_cfg_parallel: bool = True,
+            self,
+            task_type: TaskType,
+            model_id: str,
+            quant_model: bool = True,
+            local_rank: int = 0,
+            world_size: int = 1,
+            is_offload: bool = True,
+            offload_config: OffloadConfig = OffloadConfig(),
+            enable_cfg_parallel: bool = True,
     ):
         self.task_type = task_type
         self.gpu_rank = local_rank
@@ -89,7 +93,8 @@ class SkyReelsVideoSingleGpuInfer:
             rank=local_rank,
         )
         os.environ["LOCAL_RANK"] = str(local_rank)
-        logger.info(f"rank:{local_rank} Distributed backend: {dist.get_backend()}")
+        logger.info(
+            f"rank:{local_rank} Distributed backend: {dist.get_backend()}")
         torch.cuda.set_device(dist.get_rank())
         torch.backends.cuda.enable_cudnn_sdp(False)
         gpu_device = f"cuda:{dist.get_rank()}"
@@ -99,12 +104,15 @@ class SkyReelsVideoSingleGpuInfer:
         )
 
         from para_attn.context_parallel import init_context_parallel_mesh
-        from para_attn.context_parallel.diffusers_adapters import parallelize_pipe
+        from para_attn.context_parallel.diffusers_adapters import \
+            parallelize_pipe
         from para_attn.parallel_vae.diffusers_adapters import parallelize_vae
 
         max_batch_dim_size = 2 if enable_cfg_parallel and world_size > 1 else 1
         max_ulysses_dim_size = int(world_size / max_batch_dim_size)
-        logger.info(f"max_batch_dim_size: {max_batch_dim_size}, max_ulysses_dim_size:{max_ulysses_dim_size}")
+        logger.info(
+            f"max_batch_dim_size: {max_batch_dim_size}, "
+            f"max_ulysses_dim_size:{max_ulysses_dim_size}")
 
         mesh = init_context_parallel_mesh(
             self.pipe.device.type,
@@ -125,7 +133,8 @@ class SkyReelsVideoSingleGpuInfer:
         if offload_config.compiler_transformer:
             torch._dynamo.config.suppress_errors = True
             os.environ["TORCHINDUCTOR_FX_GRAPH_CACHE"] = "1"
-            os.environ["TORCHINDUCTOR_CACHE_DIR"] = f"{offload_config.compiler_cache}_{world_size}"
+            os.environ["TORCHINDUCTOR_CACHE_DIR"] = \
+                f"{offload_config.compiler_cache}_{world_size}"
             self.pipe.transformer = torch.compile(
                 self.pipe.transformer,
                 mode="max-autotune-no-cudagraphs",
@@ -140,7 +149,10 @@ class SkyReelsVideoSingleGpuInfer:
             "width": 960,
             "guidance_scale": 6,
             "num_inference_steps": 1,
-            "negative_prompt": "Aerial view, aerial view, overexposed, low quality, deformation, a poor composition, bad hands, bad teeth, bad eyes, bad limbs, distortion",
+            "negative_prompt": "Aerial view, aerial view, overexposed, "
+                               "low quality, deformation, a poor composition, "
+                               "bad hands, bad teeth, bad eyes, bad limbs, "
+                               "distortion",
             "num_frames": 97,
             "generator": torch.Generator("cuda").manual_seed(42),
             "embedded_guidance_scale": 1.0,
@@ -149,7 +161,8 @@ class SkyReelsVideoSingleGpuInfer:
             init_kwargs["image"] = Image.new("RGB", (544, 960), color="black")
         self.pipe(**init_kwargs)
 
-    def damon_inference(self, request_queue: mp.Queue, response_queue: mp.Queue):
+    def damon_inference(self, request_queue: mp.Queue,
+                        response_queue: mp.Queue):
         response_queue.put(f"rank:{self.gpu_rank} ready")
         logger.info(f"rank:{self.gpu_rank} finish init pipe")
         while True:
@@ -157,27 +170,32 @@ class SkyReelsVideoSingleGpuInfer:
             kwargs = request_queue.get()
             logger.info(f"rank:{self.gpu_rank} kwargs: {kwargs}")
             if "seed" in kwargs:
-                kwargs["generator"] = torch.Generator("cuda").manual_seed(kwargs["seed"])
+                kwargs["generator"] = torch.Generator("cuda").manual_seed(
+                    kwargs["seed"])
                 del kwargs["seed"]
             start_time = time.time()
-            assert (self.task_type == TaskType.I2V and "image" in kwargs) or self.task_type == TaskType.T2V
+            assert (
+                               self.task_type == TaskType.I2V and "image" in
+                               kwargs) or self.task_type == TaskType.T2V
             out = self.pipe(**kwargs).frames[0]
-            logger.info(f"rank:{dist.get_rank()} inference time: {time.time() - start_time}")
+            logger.info(
+                f"rank:{dist.get_rank()} inference time: "
+                f"{time.time() - start_time}")
             if dist.get_rank() == 0:
                 response_queue.put(out)
 
 
 def single_gpu_run(
-    rank,
-    task_type: TaskType,
-    model_id: str,
-    request_queue: mp.Queue,
-    response_queue: mp.Queue,
-    quant_model: bool = True,
-    world_size: int = 1,
-    is_offload: bool = True,
-    offload_config: OffloadConfig = OffloadConfig(),
-    enable_cfg_parallel: bool = True,
+        rank,
+        task_type: TaskType,
+        model_id: str,
+        request_queue: mp.Queue,
+        response_queue: mp.Queue,
+        quant_model: bool = True,
+        world_size: int = 1,
+        is_offload: bool = True,
+        offload_config: OffloadConfig = OffloadConfig(),
+        enable_cfg_parallel: bool = True,
 ):
     pipe = SkyReelsVideoSingleGpuInfer(
         task_type=task_type,
@@ -194,14 +212,14 @@ def single_gpu_run(
 
 class SkyReelsVideoInfer:
     def __init__(
-        self,
-        task_type: TaskType,
-        model_id: str,
-        quant_model: bool = True,
-        world_size: int = 1,
-        is_offload: bool = True,
-        offload_config: OffloadConfig = OffloadConfig(),
-        enable_cfg_parallel: bool = True,
+            self,
+            task_type: TaskType,
+            model_id: str,
+            quant_model: bool = True,
+            world_size: int = 1,
+            is_offload: bool = True,
+            offload_config: OffloadConfig = OffloadConfig(),
+            enable_cfg_parallel: bool = True,
     ):
         self.world_size = world_size
         smp = mp.get_context("spawn")
@@ -210,7 +228,8 @@ class SkyReelsVideoInfer:
         assert self.world_size > 0, "gpu_num must be greater than 0"
         spawn_thread = threading.Thread(
             target=self.lauch_single_gpu_infer,
-            args=(task_type, model_id, quant_model, world_size, is_offload, offload_config, enable_cfg_parallel),
+            args=(task_type, model_id, quant_model, world_size, is_offload,
+                  offload_config, enable_cfg_parallel),
             daemon=True,
         )
         spawn_thread.start()
@@ -223,14 +242,14 @@ class SkyReelsVideoInfer:
             print(f"launch_multi_gpu get init msg: {msg}")
 
     def lauch_single_gpu_infer(
-        self,
-        task_type: TaskType,
-        model_id: str,
-        quant_model: bool = True,
-        world_size: int = 1,
-        is_offload: bool = True,
-        offload_config: OffloadConfig = OffloadConfig(),
-        enable_cfg_parallel: bool = True,
+            self,
+            task_type: TaskType,
+            model_id: str,
+            quant_model: bool = True,
+            world_size: int = 1,
+            is_offload: bool = True,
+            offload_config: OffloadConfig = OffloadConfig(),
+            enable_cfg_parallel: bool = True,
     ):
         mp.spawn(
             single_gpu_run,
